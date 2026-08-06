b218eacffe1afaa5a50d4c81b60e8273395393ed
Mohamed <your-email@example.com>
2026-08-06 15:29:37 +0530

    wooos

--- main.py
+++ main.py
@@ -474,9 +474,9 @@ def score_badge(domain: str):
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return {'status':200}
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=5000, debug=False)
+    app.run(debug=False)
 
